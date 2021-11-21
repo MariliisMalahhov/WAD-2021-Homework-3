f404890b8dc69a421e5fdb858576fd5761258dc9
Bjarne Lamczak <bjl@ppi.de>
2021-11-21 19:14:41 +0200

    Text added to About Us

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Us</h1>
+
+    <div>
+      We are just a small group of three students, currently enrolled in the
+      University of Tartu. We all come from various backgrounds, bringing
+      different experiences to the team. And with the effort of each team
+      member, we thrive to reach our shared goal!
+    </div>
+
+    <br />
+
+    <div>
+      And this goal is to not just present an acceptable end-product, but one
+      that may bring to us a high grade.
+    </div>
   </div>
 </template>
